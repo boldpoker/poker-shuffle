3d419e852eac991575d4ced5ff84550c0e87a341
Holger Amann <holger@sauspiel.de>
2015-10-13 17:13:28 +0200

    Use HashDict instead of map to keep defined deck order

--- lib/shuffles.ex
+++ lib/shuffles.ex
@@ -1,16 +1,14 @@
 defmodule PokerShuffle.Shuffles do
-  @suits ["s", "c", "h", "d"]
-  @ranks ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]
-  @deck for rank <- @ranks, suit <- @suits, do: rank <> suit
+  alias PokerShuffle.PokerCards
 
-  def fisher_yates(cards \\ @deck) do
+  def fisher_yates(cards \\ PokerCards.deck) do
     card_count = Enum.count(cards)
-    Enum.reduce 0..(card_count - 1), cards, fn(i, shuffled_cards) ->
+    result = Enum.reduce 0..(card_count - 1), cards, fn(i, shuffled_cards) ->
       swap(shuffled_cards, i, rand_uniform(i, card_count))
     end
   end
 
-  def wrong(cards \\ @deck) do
+  def wrong(cards \\ PokerCards.deck) do
     card_count = Enum.count(cards)
     Enum.reduce 0..(card_count - 1), cards, fn(i, shuffled_cards) ->
       swap(shuffled_cards, i, rand_uniform(0, card_count))
